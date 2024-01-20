b781a5b7057fff14d5c24e7d87197c8fdcac67da
Parakhin Sergei <parakhin.sy@gmail.com>
2024-01-20 13:32:06 +1000

    Commit test from IDE

---
+++ InputOutput/untitled/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        var text = scanner.next();
+
+        System.out.println(text);
+    }
+}
